eccd8ad118c7d8a65598faeb429f0bd794be7090
nabin sah <nabinsah703@gmail.com>
2023-12-18 15:15:46 +0545

    create category and transaction model and connect to the sql server database using appsettings connectionStrings

---
+++ Expense Tracker/Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Expense_Tracker.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Transaction> transactions { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
+
+
+
+
--- Expense Tracker/obj/Debug/net8.0/Expense Tracker.AssemblyInfo.cs
+++ Expense Tracker/obj/Debug/net8.0/Expense Tracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Expense Tracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b91d416ee160795c64d93dd76fe0e7072cdd1871")]
 [assembly: System.Reflection.AssemblyProductAttribute("Expense Tracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Expense Tracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
