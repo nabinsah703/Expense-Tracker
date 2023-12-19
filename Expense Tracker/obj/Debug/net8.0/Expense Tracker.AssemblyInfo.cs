3c05f80c6d0225dd669aeefe72e8ae8aa6dce8d4
nabin sah <nabinsah703@gmail.com>
2023-12-19 09:14:54 +0545

    create category controller with it's View and add syncfusion component

--- Expense Tracker/obj/Debug/net8.0/Expense Tracker.AssemblyInfo.cs
+++ Expense Tracker/obj/Debug/net8.0/Expense Tracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Expense Tracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b91d416ee160795c64d93dd76fe0e7072cdd1871")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eccd8ad118c7d8a65598faeb429f0bd794be7090")]
 [assembly: System.Reflection.AssemblyProductAttribute("Expense Tracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Expense Tracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
