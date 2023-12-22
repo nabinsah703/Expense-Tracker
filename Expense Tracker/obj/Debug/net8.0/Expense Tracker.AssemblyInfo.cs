227423576a98eaf15585703fc32af0b3a861d871
nabin sah <nabinsah703@gmail.com>
2023-12-22 18:52:28 +0545

    fix the nav bar icon and side bar space

--- Expense Tracker/obj/Debug/net8.0/Expense Tracker.AssemblyInfo.cs
+++ Expense Tracker/obj/Debug/net8.0/Expense Tracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Expense Tracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34320d24dc01faf476612b01bc147d5a83feaab5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c8a7a13a752797838ccf4b0b376e32bf5d9231f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Expense Tracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Expense Tracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
